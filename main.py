a0f833e9d47f9219e988a3f88874de2a5e0dd927
A-nastya-siya <131586927+A-nastya-siya@users.noreply.github.com>
2024-07-10 13:35:08 +0300

    main

---
+++ main.py
@@ -0,0 +1,17 @@
+﻿import pandas as pd
+import random
+
+lst = ['robot'] * 10
+lst += ['human'] * 10
+random.shuffle(lst)
+data = pd.DataFrame({'whoAmI': lst})
+print(data)
+
+print('-' * 20)
+
+lst1 = ['1' if lst[i] == 'robot' else '0' for i in range(20)]
+data = data.rename(columns={'whoAmI': 'human'})
+data.loc[data['human'] == 'human', 'human'] = 1
+data.loc[data['human'] == 'robot', 'human'] = 0
+data['robot'] = lst1
+print(data)
